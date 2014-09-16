43fceaef65ea24ec1750570094b48b939f33925c
Jonathan Taratuta-Titus <taratutatitus@college.harvard.edu>
2014-09-16 00:49:01 -0400

    Using shortID to generate user IDs with a get request

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var shortid = require('shortid');
 var router = express.Router();
 
-/* GET users listing. */
+router.get('/new', newUser);
+
+function newUser(req, res) {
+	res.status(200).send(shortid.generate());
+}
+
+/* GET users listing. *//*
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
-
+*/
 module.exports = router;
