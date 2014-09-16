4ff86af8b70cbf4f156cc0804b373ad1010b4248
Michelle Deng <mdeng@college.harvard.edu>
2014-09-16 00:20:11 -0400

    latin square

--- gen_commands.py
+++ gen_commands.py
@@ -5,6 +5,10 @@ tab_names = ["font", "paragraph", "insert", "drawing", "symbols"]
 NUM_COMMANDS_PER_TAB = 12
 command_ids = range(NUM_COMMANDS_PER_TAB)
 
+def latin_square(n):
+	list = range(n)
+	return [[list[i - j] for i in list] for j in range(n, 0, -1)]
+
 def gen_commands(num_sets=3):
 	tabs = sample(tab_names, 3)
 	print "TABS: %s" % tabs
@@ -17,3 +21,4 @@ def gen_commands(num_sets=3):
 
 if __name__ == "__main__":
 	gen_commands()
+	print latin_square(3)
