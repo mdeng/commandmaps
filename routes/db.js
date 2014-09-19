f511fe32c3675e0d3912bc606ab474f793e16705
Jonathan Taratuta-Titus <taratutatitus@college.harvard.edu>
2014-09-19 18:30:03 -0400

    changed endpoints in db.js to accept additional user data at the /db/prefs endpoint

--- routes/db.js
+++ routes/db.js
@@ -4,17 +4,17 @@ var pg      = require('pg');
 var router = express.Router();
 
 router.put('/trials', processTrial);
-router.put('/prefs', processPref);
-router.put('/tlx', processTLX);
+router.put('/prefs', processUsers);
 
 /* structure of request object:
 	* userID as string
 	* interfaceType as either 'R' or 'C'
-	* commandSetId as int
-	* parentIsDiff as bool
+	* commandSetID as int
+	* needsRibbonSwitch as int
 	* time as int
-	* correct as bool
+	* hasError as int
 	* commandID as int
+	* hasRibbonError as int
 */
 function processTrial(req, res) {
 	var trialData = req.body;
@@ -43,46 +43,19 @@ function processTrial(req, res) {
 
 /* structure of request object:
    * userID as string
-   * pref as 'R' or 'C'
+   * interfaceType as 'R' or 'C'
+   * gender as 'M', 'F', 'N'
+   * age as an integer
 */
-function processPref(req, res) {
+function processUsers(req, res) {
 	var prefData = req.body;
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString = "INSERT INTO preferences VALUES ('"
+		var queryString = "INSERT INTO users VALUES ('"
 						   + prefData.userID + "', '"
-						   + prefData.interfaceType + "');";
+						   + prefData.interfaceType + "', '"
+						   + prefData.gender + "', "
+						   + prefData.age + ");";
 		console.log(queryString);
-		client.query(queryString, function(err, result) {
-			done();
-			if (err) {
-				console.error(err);
-				res.status(501).send(err);
-			} else {
-				res.send(204);
-			}
-		});
-	});
-}
-/* structure of request object:
-	* userID as string
-	* interfaceType as 'R' or 'C'
-	* mental as int
-	* physical as int
-	* temporal as int
-	* hard_work as int
-	* frustration as int
-*/
-function processTLX(req, res) {
-	var tlxData = req.body;
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString = "INSERT INTO tlx VALUES ('" +
-						   + tlxData.userID + "', '"
-						   + tlxData.interfaceType + "', "
-						   + tlxData.mental + ", "
-						   + tlxData.physical + ", "
-						   + tlxData.temporal + ", "
-						   + tlxData.hard_work + ", "
-						   + tlxData.frustration + ");";
 		client.query(queryString, function(err, result) {
 			done();
 			if (err) {
