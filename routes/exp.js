0138d497263ceda21b97b18e9eeb066530b29421
Michelle Deng <mdeng@college.harvard.edu>
2014-09-20 10:05:53 -0400

    deleted exp.js

--- routes/exp.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/experiment', function(req, res) {
-  res.render('index', { title: 'command maps wat' });
-});
-
-module.exports = router;
